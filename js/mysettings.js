d5ff71a3aec2892df7b12c21267b45772c33e534
Helder Oliveira <heldrida@gmail.com>
2014-03-28 16:47:16 +0000

    changed the settings, so that the production has its own app id from facebook and the dev its own too

--- js/mysettings.js
+++ js/mysettings.js
@@ -2,5 +2,5 @@ angular.module('mySettings', [])
 .constant('mySettings', {
 	base_path: window.location.hostname.indexOf('tlnscompetitions') > -1 ? '/emerge_space' : '/',
 	wp_base_path: window.location.hostname.indexOf('tlnscompetitions') > -1 ? '/emerge_space/wordpress' : '/wordpress',
-	app_id: 804111199618373
+	app_id: window.location.hostname.indexOf('tlnscompetitions') > -1 ? 804111199618373 : 1422154128027791
 });
