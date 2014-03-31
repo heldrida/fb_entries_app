3f933c00a1e405b5b17d520f6b1193fa8b54be33
Helder Oliveira <heldrida@gmail.com>
2014-03-31 20:32:48 +0100

    trying to identify if under fb

--- js/main.js
+++ js/main.js
@@ -160,7 +160,7 @@ angular.module("emerge_space", ['ui.router', 'jqform', 'ezfb', 'ngAnimate', 'myS
 		};
 
 		//window.parent.$("body").animate({scrollTop:0}, 'slow');
-		console.log(window.name.indexOf('app_runner_fb'));
+		console.log(window.name);
 		if(window.name.indexOf('app_runner_fb') != -1){
 			$('html, body').css('overflow', 'hidden');
 		};
